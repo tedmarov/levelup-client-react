61448d333a89a5ee6c04915a17cb6934c36412f8
ted <tedmarov@gmail.com>
2021-02-09 16:35:28 -0600

    Finished, renders, but could use links

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,26 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { GameList } from "./game/GameList.js"
+import { GameProvider } from "./game/GameProvider.js"
+import { EventList } from "./event/EventList.js"
+import { EventProvider } from "./event/EventProvider.js"
 
 export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
+            lineHeight: "1.75rem"
+        }}> 
+            <GameProvider>
+                <Route exact path="/">
+                    <GameList />
+                </Route>
+            </GameProvider>
+            <EventProvider>
+                <Route exact path="/events">
+                    <EventList />
+                </Route>
+            </EventProvider>
         </main>
     </>
 }
